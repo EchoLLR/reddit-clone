6a73207aa870512089dd0e397723daa90d18d043
Lingru Li <l379li@uwaterloo.ca>
2020-10-14 20:35:04 -0400

    Upvote and downvote APIs added

---
+++ src/main/java/com/project/redditclone/service/VoteService.java
@@ -0,0 +1,54 @@
+package com.project.redditclone.service;
+
+import com.project.redditclone.dto.VoteDto;
+import com.project.redditclone.exceptions.PostNotFoundException;
+import com.project.redditclone.exceptions.SpringRedditException;
+import com.project.redditclone.model.Post;
+import com.project.redditclone.model.Vote;
+import com.project.redditclone.repository.PostRepository;
+import com.project.redditclone.repository.VoteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.project.redditclone.model.VoteType.UPVOTE;
+
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+
+    private final VoteRepository voteRepository;
+    private final PostRepository postRepository;
+    private final AuthService authService;
+
+    @Transactional
+    public void vote(VoteDto voteDto) {
+        Post post = postRepository.findById(voteDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException("Post Not Found with ID - " + voteDto.getPostId()));
+        Optional<Vote> voteByPostAndUser = voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post, authService.getCurrentUser());
+        if (voteByPostAndUser.isPresent() &&
+                voteByPostAndUser.get().getVoteType()
+                        .equals(voteDto.getVoteType())) {
+            throw new SpringRedditException("You have already "
+                    + voteDto.getVoteType() + "'d for this post");
+        }
+        if (UPVOTE.equals(voteDto.getVoteType())) {
+            post.setVoteCount(post.getVoteCount() + 1);
+        } else {
+            post.setVoteCount(post.getVoteCount() - 1);
+        }
+        voteRepository.save(mapToVote(voteDto, post));
+        postRepository.save(post);
+    }
+
+    private Vote mapToVote(VoteDto voteDto, Post post) {
+        return Vote.builder()
+                .voteType(voteDto.getVoteType())
+                .post(post)
+                .user(authService.getCurrentUser())
+                .build();
+    }
+}
